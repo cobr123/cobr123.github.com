b2af76e6e5a122398d843f0181e16116abbae250
safebuster <safebuster2@gmail.com>
2015-04-27 14:46:07 +0300

    filters

--- by_trade_at_cities/script.js
+++ by_trade_at_cities/script.js
@@ -21,27 +21,62 @@ function changeCountry(select) {
 function changeRegion(select) {
 	document.location.href = select.value;
 }
+function getRealm(){
+	return $('#realm').val();
+}
+function getProductID(){
+	return $('#id_product').val();
+}
 //////////////////////////////////////////////////////
-function get_action(form) {
-	$.getJSON('./data.json', function (data) {
+function loadData() {
+	var realm = getRealm();
+	if (realm == null || realm == '') return;
+	var productID = '3868';//getProductID();
+	if (productID == null || productID == '') return;
+	
+	$.getJSON('./'+realm+'/tradeAtCity_'+productID+'.json', function (data) {
 		var output = '<table border="1" width="100%" cellspacing="0" cellpadding="2" bordercolorlight="#000000" bordercolordark="#FFFFFF">'
 +'<tbody><tr class="theader"><td rowspan="2">Город</td><td rowspan="2">Индекс</td><td rowspan="2">Объём</td><td rowspan="2">Местные, %</td><td colspan="3">Местные</td><td colspan="3">Магазины</td><td rowspan="2">Обновлено</td></tr>'
 +'<tr class="theader"><td>Цена</td><td>Качество</td><td>Бренд</td><td>Цена</td><td>Качество</td><td>Бренд</td></tr>';
 
 		$.each(data, function (key, val) {
-			output += '<tr class="trec">';
-			output += '<td><a href="http://virtonomica.ru/olga/main/globalreport/marketing/by_trade_at_cities/'+val.productId+'/'+val.countryId+'/'+val.regionId+'/'+val.cityId+'">'+val.cityCaption+'</a></td>';
-			output += '<td align="center">'+val.marketIdx+'</td>';
-			output += '<td align="right">'+val.volume+'</td>';
-			output += '<td align="right" style="color:black">'+val.localPercent+'</td>';
-			output += '<td align="right">'+val.localPrice+'</td>';
-			output += '<td align="right">'+val.localQuality+'</td>';
-			output += '<td align="right">'+val.localBrand+'</td>';
-			output += '<td align="right">'+val.shopPrice+'</td>';
-			output += '<td align="right">'+val.shopQuality+'</td>';
-			output += '<td align="right">'+val.shopBrand+'</td>';
-			output += '<td align="center">11.03.2013</td>';
-			output += '</tr>';
+			var suitable = true;
+			
+			if (suitable && val.volume >= $('#volumeFrom').val()) {suitable = true;} else {suitable = false;}
+			if (suitable && val.volume <= $('#volumeTo').val()) {suitable = true;} else {suitable = false;}
+			
+			if (suitable && val.localPercent >= $('#localPercentFrom').val()) {suitable = true;} else {suitable = false;}
+			if (suitable && val.localPercent <= $('#localPercentTo').val()) {suitable = true;} else {suitable = false;}
+			
+			if (suitable && val.localPrice >= $('#localPriceFrom').val()) {suitable = true;} else {suitable = false;}
+			if (suitable && val.localPrice <= $('#localPriceTo').val()) {suitable = true;} else {suitable = false;}
+			
+			if (suitable && val.localQuality >= $('#localQualityFrom').val()) {suitable = true;} else {suitable = false;}
+			if (suitable && val.localQuality <= $('#localQualityTo').val()) {suitable = true;} else {suitable = false;}
+			
+			if (suitable && val.shopPrice >= $('#shopPriceFrom').val()) {suitable = true;} else {suitable = false;}
+			if (suitable && val.shopPrice <= $('#shopPriceTo').val()) {suitable = true;} else {suitable = false;}
+			
+			if (suitable && val.shopQuality >= $('#shopQualityFrom').val()) {suitable = true;} else {suitable = false;}
+			if (suitable && val.shopQuality <= $('#shopQualityTo').val()) {suitable = true;} else {suitable = false;}
+			
+			if (suitable && val.shopBrand >= $('#shopBrandFrom').val()) {suitable = true;} else {suitable = false;}
+			if (suitable && val.shopBrand <= $('#shopBrandTo').val()) {suitable = true;} else {suitable = false;}
+			
+			if(suitable){
+				output += '<tr class="trec">';
+				output += '<td><a href="http://virtonomica.ru/olga/main/globalreport/marketing/by_trade_at_cities/'+val.productId+'/'+val.countryId+'/'+val.regionId+'/'+val.cityId+'">'+val.cityCaption+'</a></td>';
+				output += '<td align="center">'+val.marketIdx+'</td>';
+				output += '<td align="right">'+val.volume+'</td>';
+				output += '<td align="right" style="color:black">'+val.localPercent+'</td>';
+				output += '<td align="right">'+val.localPrice+'</td>';
+				output += '<td align="right">'+val.localQuality+'</td>';
+				output += '<td align="right">'+val.shopPrice+'</td>';
+				output += '<td align="right">'+val.shopQuality+'</td>';
+				output += '<td align="right">'+val.shopBrand+'</td>';
+				output += '<td align="center">11.03.2013</td>';
+				output += '</tr>';
+			}
 		});
 		output += '</tbody></table>';
 		
@@ -49,7 +84,7 @@ function get_action(form) {
 	});
 	return false;
 }
-////////////////////////////////////////////////////////////
+
 function loadProductCategories() {
 	$.getJSON('./product_categories.json', function (data) {
 		var output = '<select id="id_category" onchange="changeCategory(this);"><option value="0"></option>';
