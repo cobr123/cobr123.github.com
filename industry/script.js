16dcb901785ba4845fe1d21d35f0323f97aac88b
safebuster <safebuster2@gmail.com>
2015-05-25 19:25:55 +0300

    script.js

--- industry/script.js
+++ industry/script.js
@@ -47,7 +47,7 @@ function loadSavedFlt(){
 //резделитель разрядов
 function commaSeparateNumber(val){
 	while (/(\d+)(\d{3})/.test(val.toString())){
-		val = val.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1 ");
+		val = val.toString().replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1,");
 	}
 	return val;
 }
