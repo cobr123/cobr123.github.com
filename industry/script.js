77d732d20796690f57393168e11e8fd17266672c
safebuster <safebuster2@gmail.com>
2015-05-26 15:36:51 +0300

    ovh

--- industry/script.js
+++ industry/script.js
@@ -109,8 +109,9 @@ function updateTableFromCache(){
 	
 	tableCache.forEach(function(val){
 		output += '<tr class="trec">';
+		var openCalcHref = 'http://ovh.belyan.in/factory/'+val.manufactureID+'.html';
 		var specHref = 'http://virtonomica.ru/'+realm+'/main/industry/unit_type/info/'+val.manufactureID;
-		output += '<td align="center"><a target="_blank" href="'+specHref+'">'+val.spec+'</a></td>';
+		output += '<td align="center"><a target="_blank" href="'+specHref+'">'+val.spec+'</a>&nbsp;<a target="_blank" href="'+openCalcHref+'"><img src="../favicon.ico"></a></td>';
 		output += '<td align="center">'+val.equipQual+'</td>';
 		output += '<td align="center" id="td_tech">'+val.tech+'</td>';
 		var svMaterialsImg = '';
