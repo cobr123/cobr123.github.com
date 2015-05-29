ed88ec93b8ef0ae8b3227cd8fee22c99b766fbcf
safebuster <safebuster2@gmail.com>
2015-05-29 13:40:01 +0300

    ing qty

--- industry/script.js
+++ industry/script.js
@@ -202,7 +202,7 @@ function calcResult(recipe, materials, tech) {
 	//количество ингридиентов
 	for (var i = 0; i < num; i++) {
 		ingQuantity[i] = ingBaseQty[i] * prodbase_quan * work_quant * Math.pow(1.05, tech-1 ) * eff;
-		result.materials[i].ingQty = ingQuantity[i].toFixed(2);
+		result.materials[i].ingQty = Math.round( ingQuantity[i] );
 		//console.log('ingQuantity[i] = ' + ingQuantity[i]);
 	}
 	//цена ингридиентов
